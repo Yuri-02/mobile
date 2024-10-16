331d325418f5f94ee238acce83b13e9589947cce
Yuri Albuquerque Silva <yuri.albuquerque.2@hotmail.com>
2024-10-16 19:48:35 -0300

    Criação do Drawer Navigator

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
 import FriendDetail from './src/screens/UserDetail';
@@ -8,23 +9,37 @@ import Home from './src/screens/HomeScreen';
 import List from './src/screens/UserList';
 
 const Stack = createStackNavigator();
-export default function App() {
+const Drawer = createDrawerNavigator();
+export function StackScreen() {
   return (
-    <NavigationContainer style={styles.nav}>
-
+    
+    
       
-      <Image style={styles.logo} source={require('./img/LogoSample_ByTailorBrands (1).png')} /> 
-      <Stack.Navigator initialRouteName="Tela inicial">
+      // <Image style={styles.logo} source={require('./img/LogoSample_ByTailorBrands (1).png')} /> 
+      <Stack.Navigator >
         
         <Stack.Screen name="Tela inicial" component={Home}/>
         <Stack.Screen name="Lista amigos" component={List}/>
         <Stack.Screen name='Detalhes do amigo' component={FriendDetail}/>
       </Stack.Navigator>
-      <StatusBar style="auto" />
+      // <StatusBar style="auto" />
     
-    </NavigationContainer>
+    
   );
 }
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Tela inicial">
+        <Drawer.Screen name="Tela inicial" component={Home}/>
+        <Drawer.Screen name="Lista amigos" component={List}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
+
+
 
 const styles = StyleSheet.create({
 
